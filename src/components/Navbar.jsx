117cf86e6368d06065fe990cba2c3b62157248d8
LEANDRO DEL GIUDICE <131617887+LeandroDelGiudice@users.noreply.github.com>
2023-11-14 08:28:33 -0300

    limpieza

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { NavLink } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import { Nav } from "react-bootstrap";
 import { Navbar } from "react-bootstrap";
-import "../main.css"; // Make sure this path is correct
+import "../main.css"; 
 
 import { CartWidget } from "./cartWidget";
 
@@ -37,40 +37,3 @@ export const CustomNavbar = () => {
     </Container>
   );
 };
-/* import { NavLink } from "react-router-dom";
-import { Container } from "react-bootstrap";
-import { Nav } from "react-bootstrap";
-import { Navbar } from "react-bootstrap";
-import  "../main.css"
-
-import { CartWidget } from "./cartWidget";
-
-export const CustomNavbar = () => {
-  return (
-    
-      <Container className="justify-content-center">
-        <Navbar expand ="md" bg="light" data-bs-theme="light" >
-        <Navbar.Brand>
-        <NavLink to="/">Home</NavLink>
-        </Navbar.Brand>
-        <Nav>
-          <Nav.Link >
-          <NavLink to="category/Motores">Motores</NavLink> 
-          </Nav.Link>
-          <Nav.Link>
-          <NavLink to="category/Cajas">Cajas</NavLink>
-          </Nav.Link>
-          <Nav.Link>
-          <NavLink to="category/Diferenciales">Diferenciales</NavLink>
-          </Nav.Link>
-          
-        </Nav>
-       
-        <CartWidget />
-        
-        </Navbar>
-      </Container>
-    
-  );
-}; 
- */
